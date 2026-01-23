dd9e84764511f02efb2d36db6345c8b6bf0e0c4e
rahul21-7 <dasari2007rahul2007@gmail.com>
2026-01-23 23:27:16 +0530

    feature/projects

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "./index.css"
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import ProjectsPage from './pages/Projects/index.jsx'
